ab0d21ea3ad2b94f38cb2ea01d0bb295e5329033
Meet Rasania <meetr@hyperlinkinfosystem.net.in>
2026-04-27 18:28:35 +0530

    hi

--- lib/main.dart
+++ lib/main.dart
@@ -3,14 +3,16 @@ import 'package:flutter/services.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:toastification/toastification.dart';
-import 'features/invoice/logic/invoice_list_controller.dart';
+
 import 'features/invoice/logic/invoice_form_controller.dart';
+import 'features/invoice/logic/invoice_list_controller.dart';
 import 'features/invoice/logic/storage_service.dart';
 import 'logic/dependency_provider.dart';
 import 'presentation/pages/main_page.dart';
 import 'presentation/theme/app_theme.dart';
 
 void main() async {
+  ///CHANGES FROM MAIN BRANCH
   // Preserve the native splash until app is ready
   final binding = WidgetsFlutterBinding.ensureInitialized();
   FlutterNativeSplash.preserve(widgetsBinding: binding);
